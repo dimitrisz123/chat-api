e441add64f69249c345a622c87996dd8b1d9159c
Dimitris Zer <dimitris-007@hotmail.com>
2018-11-07 12:53:56 +0200

    initial commit

---
+++ server.js
@@ -0,0 +1,47 @@
+const Chatkit = require("@pusher/chatkit-server");
+const express = require("express");
+const bodyParser = require("body-parser");
+const { instanceLocator, key } = require("./variables.js");
+var cors = require("cors");
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+const chatkit = new Chatkit.default({
+	instanceLocator: instanceLocator,
+	key: key
+});
+
+app.post("/create_user", (req, res) => {
+	chatkit
+		.createUser({
+			id: req.body.id,
+			name: req.body.name
+		})
+		.then(response => {
+			res.json(response);
+		})
+		.catch(err => {
+			res.status(400).json("ERROR CREATING USER");
+		});
+});
+
+app.post("/auth", (req, res) => {
+	const authData = chatkit.authenticate({
+		userId: req.body.id
+	});
+
+	res.status(authData.status).send(authData.body);
+});
+
+app.post("/user", (req, res) => {
+	chatkit
+		.getUser({
+			id: req.body.id
+		})
+		.then(user => res.json(user))
+		.catch(err => res.status(400).json("USER NOT FOUND"));
+});
+
+app.listen(3000, () => console.log(`Example app listening on port 3000!`));
