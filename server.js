72dc1a0d734189e235160d13fd5da32c8cae2195
Dimitris Zer <dimitris-007@hotmail.com>
2018-11-28 22:58:29 +0200

    final

--- server.js
+++ server.js
@@ -7,12 +7,10 @@ const cors = require("cors");
 const knex = require("knex")({
 	client: "pg",
 	connection: {
-
 		host: process.env.RDS_HOSTNAME,
 		user: process.env.RDS_USERNAME,
 		password: process.env.RDS_PASSWORD,
 		database: process.env.RDS_DB_NAME
-
 	}
 });
 
