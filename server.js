0dcae1cf58a27a4cbc531514d81898cab3e152a3
Dimitris Zer <dimitris-007@hotmail.com>
2018-11-29 12:58:42 +0200

    test

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 const knex = require("knex")({
 	client: "pg",
 	connection: {
-		host: process.env.DATABASE_URL,
+		connectionString: process.env.DATABASE_URL,
 		ssl: true
 	}
 });
