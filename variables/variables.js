271d9346372f95ede15bec2f0a14f4e5da3eedd2
Dimitris Zer <dimitris-007@hotmail.com>
2018-11-29 02:01:24 +0200

    test

--- variables/variables.js
+++ variables/variables.js
@@ -2,6 +2,11 @@ const instanceLocator = process.env.INSTANCE_LOCATOR;
 
 const key = process.env.CHAT_KEY;
 
+// const instanceLocator = "v1:us1:70765878-2ac8-4e66-938d-b18c7503aded";
+
+// const key =
+// 	"0ead718b-a96c-42c7-925e-bbf19208a614:gUgIApxGgnGYLE3jFi4UZvlTe/JFdtylaCKDq0hC5hc=";
+
 module.exports = {
 	instanceLocator,
 	key
